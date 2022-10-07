7c77619a77f06401a9ffc2552bf7de815afe736b
Lucas Thom <thxmxx@gmail.com>
2022-10-07 15:32:43 -0300

    Dockerfile e k8s

--- src/serve/api.js
+++ src/serve/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 const headers = { Authorization: "Bearer " + token };
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  baseURL: '//backoffice.driveranalytics.com.br/api', //process.env.VUE_APP_API_BASE_URL,
   headers,
 });
 export default api;
